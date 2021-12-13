76eccb0306734047d40ca2a9bcea0ffc9b6e0078
eswarvankayala <63575589+eswarvankayala@users.noreply.github.com>
2021-12-13 23:51:36 +0530

    Create positive number in list.py

---
+++ positive number in list.py
@@ -0,0 +1,14 @@
+list1=[12,-7,5,64,-14]
+pos_list1=[]
+print("list1=",list1)
+for i in list1:
+  if(i>0):
+    pos_list1.append(i)
+print(pos_list1)
+list2=[12,14,-95,3]
+pos_list2=[]
+print("list2=",list2)
+for i in list2:
+  if(i>0):
+    pos_list2.append(i)
+print(pos_list2)
