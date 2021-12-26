91c083490f3180c732c146e322a052d31e0eacf8
eswarvankayala <63575589+eswarvankayala@users.noreply.github.com>
2021-12-26 14:22:24 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,9 +2,10 @@ a=0
 b=1
 print(a)
 print(b)
+k=int(input("enter the required the length tof thr series"))
 for i in range(0,100):
-   c=a+b
-   print(c)
-   a=b
-	 b=c
+    c=a+b
+    print(c)
+    a=b
+    b=c
     
